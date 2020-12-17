dcafef296953caf1e3a65edceb46ba14b142fd47
k18019kk <k18019kk@PCK18019KK.local>
2020-12-17 22:26:15 +0900

    schedule_app

---
+++ routes/sche.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
+const { Op } = require('sequelize');
+
+const pnum = 10;
+
+// ログインチェックの関数
+function check(req,res) {
+    if (req.session.login == null) {
+      req.session.back = '/sche';
+      res.redirect('/users/login');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  router.get('/', (req, res, next)=> {
+    if (check(req,res)){ return };
+    db.Schedule.findAll({
+      where:{userId: req.session.login.id},
+      limit:pnum,
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    }).then(sche=> {
+      var data = {
+        title: 'スケジュール管理アプリ',
+        login: req.session.login,
+        message: '※最近の投稿データ',
+        form: {find:''},
+        content:sche
+      };
+      res.render('sche/index', data);    
+    });
+  });
+
+  router.get('/add', (req, res, next) => {
+    if (check(req,res)){ return };
+    res.render('sche/add', { title: 'スケジュール追加' });
+  });
+  
+  // 新規作成フォームの送信処理
+  router.post('/add', (req, res, next) => {
+    if (check(req,res)){ return };
+    db.sequelize.sync()
+    .then(() => db.Schedule.create({
+      userId: req.session.login.id,
+      begin: req.body.begin,
+      end: req.body.end,
+      place: req.body.place,
+      content: req.body.content,
+    })
+    .then(model => {
+       res.redirect('/sche');
+    })
+    );
+  });
+
+// router.get('/add',(req, res, next)=> {
+//     var data = {
+//       title: 'スケジュール追加',
+//       form: new db.Schedule(),
+//       err:null
+//     }
+//     res.render('sche/add', data);
+//   });
+  
+//   router.post('/add',(req, res, next)=> {
+//     const form = {
+//       begin: req.body.begin,
+//       end: req.body.end,
+//       place: req.body.place,
+//       content: req.body.content
+//     };
+//     db.sequelize.sync()
+//       .then(() => db.Schedule.create(form)
+//       .then(sche=> {
+//         res.redirect('/sche')
+//       })
+//       .catch(err=> {
+//         var data = {
+//           title: 'スケジュール追加',
+//           form: form,
+//           err: err
+//         }
+//         res.render('sche/add', data);
+//       })
+//       )
+//   });
+
+router.get('/edit',(req, res, next)=> {
+  db.Schedule.findByPk(req.query.id)
+  .then(sche => {
+    var data = {
+      title: 'スケジュール編集',
+      form: sche
+    }
+    res.render('sche/edit', data);
+  });
+});
+
+router.post('/edit',(req, res, next)=> {
+  db.Schedule.findByPk(req.body.id)
+  .then(sche => {
+    sche.begin = req.body.begin;
+    sche.end = req.body.end;
+    sche.place = req.body.place;
+    sche.content = req.body.content;
+    sche.save().then(()=>res.redirect('/sche'));
+  });
+});
+
+router.get('/delete',(req, res, next)=> {
+  db.Schedule.findByPk(req.query.id)
+  .then(sche => {
+    var data = {
+      title: 'スケジュール削除',
+      form: sche
+    }
+    res.render('sche/delete', data);
+  });
+});
+
+router.post('/delete',(req, res, next)=> {
+  db.Schedule.findByPk(req.body.id)
+  .then(sche => {
+    sche.destroy().then(()=>res.redirect('/sche'));
+  });
+});
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/index');
+
+/* GET users listing. */
+const { Op } = require("sequelize");
+
+/* GET users listing. */
+router.get('/',(req, res, next)=> {
+  db.User.findAll().then(usrs => {
+    var data = {
+      title: 'Users/Index',
+      content: usrs
+    }
+    res.render('users/index', data);
+  });
+});
+
+module.exports = router;
+
+router.get('/add',(req, res, next)=> {
+  var data = {
+    title: 'Users/Add',
+    form: new db.User(),
+    err:null
+  }
+  res.render('users/add', data);
+});
+
+router.post('/add',(req, res, next)=> {
+  const form = {
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age
+  };
+  db.sequelize.sync()
+    .then(() => db.User.create(form)
+    .then(usr=> {
+      res.redirect('/users')
+    })
+    .catch(err=> {
+      var data = {
+        title: 'Users/Add',
+        form: form,
+        err: err
+      }
+      res.render('users/add', data);
+    })
+    )
+});
+
+router.get('/edit',(req, res, next)=> {
+  db.User.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Edit',
+      form: usr
+    }
+    res.render('users/edit', data);
+  });
+});
+
+router.post('/edit',(req, res, next)=> {
+  db.User.findByPk(req.body.id)
+  .then(usr => {
+    usr.name = req.body.name;
+    usr.pass = req.body.pass;
+    usr.mail = req.body.mail;
+    usr.age = req.body.age;
+    usr.save().then(()=>res.redirect('/users'));
+  });
+});
+
+router.get('/delete',(req, res, next)=> {
+  db.User.findByPk(req.query.id)
+  .then(usr => {
+    var data = {
+      title: 'Users/Delete',
+      form: usr
+    }
+    res.render('users/delete', data);
+  });
+});
+
+router.post('/delete',(req, res, next)=> {
+  db.User.findByPk(req.body.id)
+  .then(usr => {
+    usr.destroy().then(()=>res.redirect('/users'));
+  });
+});
+
+router.get('/login', (req, res, next) => {
+  var data = {
+     title:'Users/Login',
+     content:'名前とパスワードを入力下さい。'
+  }
+  res.render('users/login', data);
+});
+
+router.post('/login', (req, res, next) => {
+  db.User.findOne({
+    where:{
+      name:req.body.name,
+      pass:req.body.pass,
+    }
+  }).then(usr=>{
+    if (usr != null) {
+      req.session.login = usr;
+      let back = req.session.back;
+      if (back == null){
+        back = '/';
+      }
+      res.redirect(back);
+    } else {
+      var data = {
+        title:'Users/Login',
+        content:'名前かパスワードに問題があります。再度入力下さい。'
+      }
+      res.render('users/login', data);
+    }
+  })
+});
+
+module.exports = router;
